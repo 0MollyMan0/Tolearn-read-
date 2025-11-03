febad34d37c5ef0f98101dfa7174c0b096fea64d
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-11-03 12:52:01 +0100

    ex03

--- ex03.c
+++ ex03.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ex02.c                                             :+:      :+:    :+:   */
+/*   ex03.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/03 12:01:39 by anfouger          #+#    #+#             */
-/*   Updated: 2025/11/03 12:39:35 by anfouger         ###   ########.fr       */
+/*   Created: 2025/11/03 12:38:17 by anfouger          #+#    #+#             */
+/*   Updated: 2025/11/03 12:50:43 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,25 @@
 #include <stdio.h>
 
 int main() {
-    int fd = open("text.txt", O_RDONLY);
+    int fd;
+	ssize_t bytes;
+	char buffer[11];
+
+	fd = open("text.txt", O_RDONLY);
     if (fd == -1)
 	{ 
 		printf("open error");
 		return 1;
 	}
-    char buffer[11];
-    ssize_t bytes = read(fd, buffer, 20);
-    if (bytes == -1) {
-        printf("read error");
-        return 1;
-    }
-
-    buffer[bytes] = '\0';
-    printf("Read %zd bytes: %s\n", bytes, buffer);
-
+	while ((bytes = read(fd, buffer, 10)) > 0)
+	{
+		buffer[bytes] = '\0';
+		write(1, buffer, bytes);
+	}
+	if (bytes == -1)
+	{
+		printf("read error\n");
+	}
     close(fd);
     return 0;
 }

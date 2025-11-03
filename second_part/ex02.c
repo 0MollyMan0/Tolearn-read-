aa6cea7106d43bb60919983f2c744e252971ac1d
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-11-03 15:47:03 +0100

    ex02 part2

--- second_part/ex02.c
+++ second_part/ex02.c
@@ -6,7 +6,7 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 15:34:32 by anfouger          #+#    #+#             */
-/*   Updated: 2025/11/03 15:34:42 by anfouger         ###   ########.fr       */
+/*   Updated: 2025/11/03 15:46:35 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_n(const char *file, int n)
+void print_all(const char *name, int n)
 {
-	
+	int file;
+	ssize_t bytes;
+	char *buffer;
+
+	file = open(name, O_RDONLY);
+	buffer = malloc(n);
+	if (file == -1 || !buffer)
+	{
+		if (file == -1)
+			return;
+		close(file);
+		return;
+	}
+	while((bytes = read(file, buffer, n)) > 0)
+		write(1, buffer, bytes);
+	if (bytes == -1)
+		write(1, "read error", 10);
+	else
+		write(1, "\n\nend of file\n", 14);
+	close(file);
 }
 
 int main(int argc, char const *argv[])
@@ -27,6 +46,6 @@ int main(int argc, char const *argv[])
         write(2, "Usage: ./a.out <file> <n>\n", 27);
         return (1);
     }
-    print_n(argv[1], atoi(argv[2]));
+    print_all(argv[1], atoi(argv[2]));
     return (0);
 }
